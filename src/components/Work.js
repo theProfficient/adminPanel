4678069f4238e890ee86e1d2fa2d2a89466dd9fd
dipalidatir-demo <106646972+dipalidatir-demo@users.noreply.github.com>
2023-01-13 10:42:17 +0530

    ff

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,29 +1,25 @@
 import "./WorkCardStyle.css"
 import WorkCard from "./WorkCard"
 import WorkCardData from "./WorkCardData"
-
-
 import React from 'react'
-
 
 const Work = () => {
   return (
     <div className="work-container">
         <h1 className="project-heading">Projects</h1>
         <div className="project-container">
-            {WorkCardData.map((val,ind) =>{
-return(
-    <WorkCard
-    key={ind}
-    imgsrc={val.imgsrc}
-    title={val.title}
-    text={val.text}
-    view={val.view}
-    />
-);
-
+            {WorkCardData.map((val,ind)=>{
+                return (
+                    <WorkCard key={ind}
+                    pc={val.imgsrc}
+                    title={val.title}
+                    text={val.text}
+                    view={val.view}
+                    />
+                    
+                )
             })}
-            </div>
+        </div>
     </div>
   )
 }
--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,21 +1,15 @@
-// type rafce for format of react function it will get filename also
-
 import React from 'react'
+import Navbar from '../components/Navbar' 
 import Footer from '../components/Footer'
 import HeroImage2 from '../components/HeroImage2'
-import Navbar from '../components/Navbar'
-import Form from "../components/Form"
-
-
+import Form from '../components/Form'
 const Contact = () => {
   return (
     <div>
       <Navbar/>
-      <HeroImage2 heading="CONTACT." text="Let's have a Chat"/>
-      <form/>
+      <HeroImage2 heading="CONTACT." text="Let's have a chat."/>
+      <Form/>
       <Footer/>
-
-
     </div>
   )
 }
